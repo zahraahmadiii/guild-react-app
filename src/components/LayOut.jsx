8bb15793093d15dc67ed6ab111238a26c7c33ab7
زهرا احمدی <3920659791@ittadbir.local>
2024-12-25 14:55:11 +0330

    install packages

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import '../style.css'
 const LayOut = () => {
   return (
-    <div>LayOut</div>
+    <div className='layOut'>
+      <div className='side-show'>
+        <p className='title'>ثبت نام شاغلین در کسب و کارها</p>
+      </div>
+      <div className='content'>
+        <div className='top'>
+           <div className='progress-wrapper'>
+            <ul>
+              <li>
+              <span>1</span>
+              </li>
+              <li>
+              <span>2</span>
+              </li>
+              <li>   
+              <span>3</span>
+              </li>
+            </ul>
+           </div>
+        </div>
+       <MultiStep/>
+      </div>
+    </div>
   )
 }
 
